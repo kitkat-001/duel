a3408012ba6a26bdca7b1d3598686ba81fd3d4d9
kitkat-001 <54290919+kitkat-001@users.noreply.github.com>
2023-11-14 21:04:35 -0500

    added raycast

--- duel_rust/src/player.rs
+++ duel_rust/src/player.rs
@@ -1,12 +1,13 @@
 use godot::prelude::*;
 use godot::prelude::real_consts::FRAC_PI_2;
-use godot::engine::{INode3D, Node3D, InputEvent, InputEventMouseMotion};
+use godot::engine::{INode3D, Node3D, InputEvent, InputEventAction, InputEventMouseMotion, RayCast3D};
 use godot::engine::utilities::clampf;
 
 #[derive(GodotClass)]
 #[class(base=Node3D)]
 struct Player {
     camera: Option<Gd<Camera3D>>,
+    raycast: Option<Gd<RayCast3D>>,
     delta: real,
 
     #[base]
@@ -17,18 +18,49 @@ struct Player {
 impl INode3D for Player {
     fn init(base: Base<Node3D>) -> Self {
         Self {
-            delta: 0f32, 
             camera: None,
+            raycast: None,
+            delta: 0f32, 
             base
         } 
     }
 
     fn process(&mut self, delta: f64) {
+        if self.camera.is_none() || self.raycast.is_none() { self.set_nodes(); }
         self.delta = delta as f32;
     }
 
     fn input(&mut self, event: Gd<InputEvent>) {
-        if self.camera.is_none() { self.set_camera(); }
+        self.set_camera_rotation(event)
+
+    }
+}
+
+impl Player {
+    fn set_nodes(&mut self) {
+        if self.base.get_child_count() > 0 {
+            let cam_node = self.base.get_child(0);
+            self.camera = {
+                if let Some(node) = cam_node {
+                    node.try_cast::<Camera3D>()
+                } else {
+                    None
+                }
+            };
+        }
+        if self.base.get_child_count() > 1 {
+            let raycast_node = self.base.get_child(1);
+            self.raycast = {
+                if let Some(node) = raycast_node {
+                    node.try_cast::<RayCast3D>()
+                } else {
+                    None
+                }
+            };
+        }
+    }
+
+    fn set_camera_rotation(&mut self, event: Gd<InputEvent>) {
         if let Some(camera) = &mut self.camera {
             let mouse_event = event.try_cast::<InputEventMouseMotion>();
             if let Some(mouse_event) = mouse_event {
@@ -42,19 +74,13 @@ impl INode3D for Player {
             }
         }
     }
-}
 
-impl Player {
-    fn set_camera(&mut self) {
-        if self.base.get_child_count() > 0 {
-            let cam_node = self.base.get_child(0);
-            self.camera = {
-                if let Some(node) = cam_node {
-                    node.try_cast::<Camera3D>()
-                } else {
-                    None
-                }
-            };
+    fn shoot(&self, event: Gd<InputEvent>) {
+        let mouse_event = event.try_cast::<InputEventAction>();
+        if let Some(mouse_event) = mouse_event {
+            if mouse_event.action() == StringName::from("shoot") {
+                
+            }
         }
     }
 }
